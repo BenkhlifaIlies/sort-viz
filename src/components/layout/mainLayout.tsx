cb14e54b0b82fc7314744fc85692197224a587bc
BenkhlifaIlies <50270680+BenkhlifaIlies@users.noreply.github.com>
2023-08-04 05:55:05 +0100

    add footer component

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import Footer from '../footer';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const mainLayout = ({ children }: LayoutProps) => {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      {children}
+      <Footer />
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
