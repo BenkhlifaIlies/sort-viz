66f9ec0ece8efd218feeefa970809482655e5deb
BenkhlifaIlies <50270680+BenkhlifaIlies@users.noreply.github.com>
2023-08-04 03:13:18 +0100

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+
 import './App.css';
 import MainLayout from './components/layout/mainLayout';
-
-function App() {
+function MyApp() {
   return (
     <MainLayout>
       <h1>Hello World!!</h1>
     </MainLayout>
   );
 }
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MyApp />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
