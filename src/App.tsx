4b5e40d14da17adec1290a788a988d9ec855609c
BenkhlifaIlies <50270680+BenkhlifaIlies@users.noreply.github.com>
2023-08-11 02:08:33 +0100

    add modal component

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,52 @@ import {
 
 import './App.css';
 import MainLayout from './components/layout/mainLayout';
+import { useState } from 'react';
+import Modal from './components/common/modal';
 function MyApp() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <MainLayout>
-      <h1>Hello World!!</h1>
+      <h1
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Hello World!!
+      </h1>
+      <h1
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Hello World!!
+      </h1>
+      <h1
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Hello World!!
+      </h1>
+      <h1
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Hello World!!
+      </h1>
+      {modalOpen && (
+        <Modal title={'test'} setOpenModal={setModalOpen}>
+          <Modal.Body>
+            <h1>Hello from modal's body</h1>
+            <h1>Hello from modal's body again</h1>
+          </Modal.Body>
+          <Modal.Footer>
+            <h1>Hello from modal's footer</h1>
+          </Modal.Footer>
+        </Modal>
+      )}
     </MainLayout>
   );
 }
