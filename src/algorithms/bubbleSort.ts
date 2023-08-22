1d75ac8d4651a3b2367d99b0dd19dc95f18444af
BenkhlifaIlies <50270680+BenkhlifaIlies@users.noreply.github.com>
2023-08-22 05:06:33 +0100

    remove unnecessary console log

--- src/algorithms/bubbleSort.ts
+++ src/algorithms/bubbleSort.ts
@@ -5,7 +5,6 @@ export default async function bubbleSort(
   speed: number,
   updateValues: (array: BarElement[]) => void,
 ) {
-  console.log(arr.length, speed);
   const sorter = await bubbleSorter();
   sorter(arr, speed, updateValues);
 }
